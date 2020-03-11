1b5d0e05dbf4a4c7a07533415e9056fa376d4c86
Michaella Babbitt <tinydynamine@gmail.com>
2020-03-10 20:55:16 -0500

    first commit, added all starter components(home, login, admin), and an authorization service

---
+++ src/app/authorization.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorizationService {
+
+  constructor() { }
+
+  getUserDetails(){
+    //post these details to api server & return userinfo if correct
+  }
+}
